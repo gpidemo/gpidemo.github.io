1ea5e48be0324a73e37994d15fb497156c8fd5c3
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-08-12 11:30:05 -0400

    Remove 2 second timeout

--- pr/apps/origin-test/pr.js
+++ pr/apps/origin-test/pr.js
@@ -95,16 +95,14 @@ function onBuyClicked() { // eslint-disable-line no-unused-vars
     try {
         request.show()
             .then(function(instrumentResponse) {
-                window.setTimeout(function() {
-                    instrumentResponse.complete('success')
-                        .then(function() {
-                            done('This is a demo website. No payment will be processed.', instrumentResponse);
-                        })
-                        .catch(function(err) {
-                            error(err);
-                            request = buildPaymentRequest();
-                        });
-                }, 2000);
+                instrumentResponse.complete('success')
+                    .then(function() {
+                        done('This is a demo website. No payment will be processed.', instrumentResponse);
+                    })
+                    .catch(function(err) {
+                        error(err);
+                        request = buildPaymentRequest();
+                    });
             })
             .catch(function(err) {
                 error(err);
