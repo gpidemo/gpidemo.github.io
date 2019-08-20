b887fc1ad8ebcf7a960f2a30c885bb028327d020
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-08-20 21:48:26 +0200

    Update app.js

--- pr/apps/origin/app.js
+++ pr/apps/origin/app.js
@@ -11,12 +11,21 @@ self.addEventListener('message', (evt) => {
       methodName: self.method,
       details: {        	
         uetr: '972a99ab-46e8-4fbd-ae6e-77cf56909dc2',
-        status: 'ACCP',
       },
     });
     evt.openWindow('getstatus.html#' + evt.details.uetr);
     self.resolver = null;
   } else {
+  if (evt.data === 'getstatus' && self.resolver !== null) {
+    self.resolver({
+      methodName: self.method,
+      details: {        	
+        status: 'ACCP',
+      },
+    });
+    evt.openWindow('getstatus.html#' + evt.details.uetr);
+    self.resolver = null;
+  }} else {
     console.log('Unrecognized message: ' + evt.data);
   }
 });
