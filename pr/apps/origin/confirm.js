3bd4bac7d456e3ef6253b47abf9aeffcbb751f05
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-08-21 20:11:41 +0200

    update

--- pr/apps/origin/confirm.js
+++ pr/apps/origin/confirm.js
@@ -4,6 +4,7 @@ button.addEventListener('click', (evt) => {
   button.style.display = 'none';
   pleasewait.style.display = 'block';
   navigator.serviceWorker.controller.postMessage('confirm');
+  window.location.href = 'getstatus.html'
 });
 const urlParts = window.location.href.split('#');
 if (urlParts.length === 5) {
