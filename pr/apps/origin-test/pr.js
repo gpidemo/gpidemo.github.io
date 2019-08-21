76ac235c2d393883b0afa331a7f44d7883471c5f
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-08-21 21:15:07 +0200

    Update pr.js

--- pr/apps/origin-test/pr.js
+++ pr/apps/origin-test/pr.js
@@ -107,7 +107,7 @@ function onBuyClicked() { // eslint-disable-line no-unused-vars
                 instrumentResponse.complete('success')
                     .then(function() {
  //                       done('This is a demo website. No payment will be processed.', instrumentResponse);
-                        done(window.location.href = 'https://gpidemo.github.io/pr/apps/origin/getstatus.html', instrumentResponse);
+                        done(window.location.href = 'https://gpidemo.github.io/pr/apps/origin/getstatus.html#' + instrumentResponse.details.UETR, instrumentResponse);
                     })
                     .catch(function(err) {
                         error(err);
