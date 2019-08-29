57478dc56ea1caa41fa743e6bffc7dc2ff968072
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-08-30 01:02:23 +0200

    Update pr.js

--- pr/apps/origin-test/pr.js
+++ pr/apps/origin-test/pr.js
@@ -162,6 +162,11 @@ function onBuyClicked() { // eslint-disable-line no-unused-vars
 }
 
 
+// Popup window function
+function basicPopup(url) {
+  popupWindow = window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+}
+
 function uuid()
 {
     var seed = Date.now();
