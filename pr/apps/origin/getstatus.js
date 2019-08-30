280f6d6b06aed3bd76b36a1d779bd75d0045cdf7
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-08-30 20:18:42 +0200

    Update getstatus.js

--- pr/apps/origin/getstatus.js
+++ pr/apps/origin/getstatus.js
@@ -3,7 +3,34 @@ const spleasewait = document.getElementById('pleasewait');
 sbutton.addEventListener('click', (evt) => {
   sbutton.style.display = 'none';
   spleasewait.style.display = 'block';
-  navigator.serviceWorker.controller.postMessage('getstatus');
+//  navigator.serviceWorker.controller.postMessage('getstatus');
+var activeUetr = document.getElementById('uetr');
+console.log('Active UETR:', activeUetr);
+fetch('https://u6b176ktza.execute-api.eu-west-1.amazonaws.com/test/glink/payment_initiation/' + activeUetr + '/tracker_status', {
+  method: 'GET',
+  headers: new Headers({
+    "Content-Type": "application/json",
+    "x-api-key": methodData.data.apiKey,
+    })
+  })
+  .then((response) => {
+    console.log('GetStatus', response);
+    if (response.ok) {
+      return response.json();
+    }
+    respond('failure', '');
+    })
+  .then((jsonResponse) => {
+      //console.log(jsonResponse);
+      console.log('Body:', jsonResponse);
+      console.log('Status UETR:', jsonResponse.uetr);
+      document.getElementById('status').innerHTML = jsonResponse.status;
+    })
+  .catch((err) => {
+       console.log(err);
+       respond('failure', JSON.stringify(err));
+    });
+  
 });
 const urlParts = window.location.href.split('#');
 if (urlParts.length === 2) {
