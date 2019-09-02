eff71c01781b60a608289600f8a6ae7a25b8452b
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-09-02 15:06:22 +0200

    Update

--- pr/apps/origin/getstatus.js
+++ pr/apps/origin/getstatus.js
@@ -1,5 +1,5 @@
 const gbutton = document.getElementById('getstatus');
-const gpleasewait = document.getElementById('pleasewait');
+const gpleasewait = document.getElementById('spleasewait');
 gbutton.addEventListener('click', (evt) => {
   gbutton.style.display = 'none';
   gpleasewait.style.display = 'block';
@@ -7,46 +7,17 @@ gbutton.addEventListener('click', (evt) => {
 
 });
 
-  const sbutton = document.getElementById('setstatus');
-  const spleasewait = document.getElementById('pleasewait');
-  sbutton.addEventListener('click', (evt) => {
-    sbutton.style.display = 'none';
-    spleasewait.style.display = 'block';
-    navigator.serviceWorker.controller.postMessage('setstatus');
-    /*var activeUetr = document.getElementById('uetr').innerHTML;
-console.log('Active UETR:', activeUetr);
-
-apiKey = localStorage.getItem("apikey");
-
-
-
-fetch('https://u6b176ktza.execute-api.eu-west-1.amazonaws.com/test/glink/payment_initiation/' + activeUetr + '/tracker_status', {
-  method: 'GET',
-  headers: new Headers({
-    "Content-Type": "application/json",
-    "x-api-key": methodData.data.apiKey,
-    })
-  })
-  .then((response) => {
-    console.log('GetStatus', response);
-    if (response.ok) {
-      return response.json();
-    }
-    respond('failure', '');
-    })
-  .then((jsonResponse) => {
-      //console.log(jsonResponse);
-      console.log('Body:', jsonResponse);
-      console.log('Status UETR:', jsonResponse.uetr);
-      document.getElementById('status').innerHTML = jsonResponse.uetr;
-    })
-  .catch((err) => {
-       console.log(err);
-       respond('failure', JSON.stringify(err));
-    });
-*/  
+const sbutton = document.getElementById('setstatus');
+const spleasewait = document.getElementById('spleasewait');
+sbutton.addEventListener('click', (evt) => {
+  sbutton.style.display = 'none';
+  spleasewait.style.display = 'block';
+  navigator.serviceWorker.controller.postMessage('setstatus');
 }); 
-const urlParts = window.location.href.split('#');
+
+
+
+    const urlParts = window.location.href.split('#');
 if (urlParts.length === 3) {
   document.getElementById('uetr').innerHTML = urlParts[1];
   document.getElementById('status').innerHTML = urlParts[2];
