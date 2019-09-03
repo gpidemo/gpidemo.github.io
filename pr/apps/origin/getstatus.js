eeb893057f4a810fc14aa6d03cb296d268ad3d06
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-09-03 15:35:38 +0200

    Update getstatus.js

--- pr/apps/origin/getstatus.js
+++ pr/apps/origin/getstatus.js
@@ -3,13 +3,14 @@ const gpleasewait = document.getElementById('gpleasewait');
 const sbutton = document.getElementById('setstatus');
 const spleasewait = document.getElementById('spleasewait');
 
+var statusChannel = new MessageChannel ();
+statusChannel.port1.onmessage = statusReply();
 
 gbutton.addEventListener('click', (evt) => {
   gbutton.style.display = 'none';
   gpleasewait.style.display = 'block';
   console.log ('Get Status window:' , document.getElementById('uetr').innerHTML)
-  var statusChannel = new MessageChannel ();
-  statusChannel.port1.onmessage = statusReply();
+
   navigator.serviceWorker.controller.postMessage(
     {command : 'getstatus', details : document.getElementById('uetr').innerHTML}, 
     [statusChannel.port2]);
@@ -19,8 +20,7 @@ sbutton.addEventListener('click', (evt) => {
   sbutton.style.display = 'none';
   spleasewait.style.display = 'block';
   console.log ('Set Status window:' , document.getElementById('uetr').innerHTML)
-  var statusChannel = new MessageChannel ();
-  statusChannel.port1.onmessage = statusReply();
+ 
   navigator.serviceWorker.controller.postMessage(
     {command : 'setstatus', details : document.getElementById('uetr').innerHTML}, 
     [statusChannel.port2]);
@@ -29,12 +29,13 @@ sbutton.addEventListener('click', (evt) => {
 
 function statusReply (evt)  {
   console.log ('Reply Status event:', evt);
-  document.getElementById('uetr').innerHTML = evt.uetr;
-  document.getElementById('status').innerHTML = evt.status;
+  document.getElementById('uetr').innerHTML = evt.data.uetr;
+  document.getElementById('status').innerHTML = evt.data.status;
   gbutton.style.display = 'block';
   gpleasewait.style.display = 'none';
   sbutton.style.display = 'block';
   spleasewait.style.display = 'none';
+  statusChannel.port2.close;
 };
 
 
