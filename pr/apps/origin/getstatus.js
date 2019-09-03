4392ccf7bae3609c02d7c71bdace3075c2912e8d
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-09-03 22:27:41 +0200

    Update getstatus.js

--- pr/apps/origin/getstatus.js
+++ pr/apps/origin/getstatus.js
@@ -30,6 +30,28 @@ sbutton.addEventListener('click', (evt) => {
     [setstatusChannel.port2]);
 }); 
 
+function sendMessage(message) {
+  // This wraps the message posting/response in a promise, which will
+  // resolve if the response doesn't contain an error, and reject with
+  // the error if it does. If you'd prefer, it's possible to call
+  // controller.postMessage() and set up the onmessage handler
+  // independently of a promise, but this is a convenient wrapper.
+  return new Promise(function(resolve, reject) {
+    var statusChannel = new MessageChannel();
+    statusChannel.port1.onmessage = function(event) {
+      if (event.data.error) {
+        reject(event.data.error);
+      } else {
+        resolve(event.data);
+      }
+    };
+    navigator.serviceWorker.controller.postMessage(message, [statusChannel.port2]);
+  });
+}
+
+
+
+
 
 function statusReply (evt)  {
   if ((evt !== null) && (evt !== undefined)) {
