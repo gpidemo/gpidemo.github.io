cac132369ea9c39e9eeaf8c0cbcc908d964577f7
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-09-04 23:45:15 +0200

    Update

--- pr/apps/origin/confirm.js
+++ pr/apps/origin/confirm.js
@@ -10,33 +10,38 @@ button.addEventListener('click', (evt) => {
 
 const urlParts = window.location.href.split('#');
 if (urlParts.length === 5) {
+ 
+ console.log (urlParts);
   document.getElementById('currency').innerHTML = urlParts[1];
   document.getElementById('value').innerHTML = urlParts[2];
   document.getElementById('creditorName').innerHTML = urlParts[3];
   document.getElementById('creditorAccount').innerHTML = urlParts[4];
 
-  console.log (debtorAccountString);
  
+/*
   document.getElementById('debtor1Name').innerHTML = debtorAccountString[0].accountNumber
   document.getElementById('debtor1Account').innerHTML = debtorAccountString[0].accountNumber;
-  document.getElementById('debtor1PartyID').innerHTML = debtorAccountString[0].accountOwnerId;
+  document.getElementById('debtor1PartyId').innerHTML = debtorAccountString[0].accountOwnerId;
   document.getElementById('debtor1BIC').innerHTML = debtorAccountString[0].bankIdentifier;
 
   document.getElementById('debtor2Name').innerHTML = debtorAccountString[1].accountOwner;
   document.getElementById('debtor2Account').innerHTML = debtorAccountString[1].accountNumber;
-  document.getElementById('debtor2PartyID').innerHTML = debtorAccountString[1].accountOwnerId;
+  document.getElementById('debtor2PartyId').innerHTML = debtorAccountString[1].accountOwnerId;
   document.getElementById('debtor2BIC').innerHTML = debtorAccountString[1].bankIdentifier;
+*/
 };
 
+
 function completeAccount () {
+  console.log (debtorAccountString);
   document.getElementById('debtor1Name').innerHTML = debtorAccountString[0].accountOwner;
   document.getElementById('debtor1Account').innerHTML = debtorAccountString[0].accountNumber;
-  document.getElementById('debtor1PartyID').innerHTML = debtorAccountString[0].accountOwnerId;
+  document.getElementById('debtor1PartyId').innerHTML = debtorAccountString[0].accountOwnerId;
   document.getElementById('debtor1BIC').innerHTML = debtorAccountString[0].bankIdentifier;
 
   document.getElementById('debtor2Name').innerHTML = debtorAccountString[1].accountOwner;
   document.getElementById('debtor2Account').innerHTML = debtorAccountString[1].accountNumber;
-  document.getElementById('debtor2PartyID').innerHTML = debtorAccountString[1].accountOwnerId;
+  document.getElementById('debtor2PartyId').innerHTML = debtorAccountString[1].accountOwnerId;
   document.getElementById('debtor2BIC').innerHTML = debtorAccountString[1].bankIdentifier;
 };
 
