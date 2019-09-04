ea6d5bbfe7c9f3a670591552a5234f967bebefe1
gpidemo <53574397+gpidemo@users.noreply.github.com>
2019-09-04 22:52:06 +0200

    Update confirm.js

--- pr/apps/origin/confirm.js
+++ pr/apps/origin/confirm.js
@@ -15,29 +15,29 @@ if (urlParts.length === 5) {
   document.getElementById('creditorName').innerHTML = urlParts[3];
   document.getElementById('creditorAccount').innerHTML = urlParts[4];
 
-  console.log (debitAccount);
+  console.log (debtorAccountString);
  
-  document.getElementById('debtor1Name').innerHTML = debitAccount[0].accountOwner;
-  document.getElementById('debtor1Account').innerHTML = debitAccount[0].accountNumber;
-  document.getElementById('debtor1PartyID').innerHTML = debitAccount[0].accountOwnerId;
-  document.getElementById('debtor1BIC').innerHTML = debitAccount[0].bankIdentifier;
+  document.getElementById('debtor1Name').innerHTML = debtorAccountString[0].accountNumber
+  document.getElementById('debtor1Account').innerHTML = debtorAccountString[0].accountNumber;
+  document.getElementById('debtor1PartyID').innerHTML = debtorAccountString[0].accountOwnerId;
+  document.getElementById('debtor1BIC').innerHTML = debtorAccountString[0].bankIdentifier;
 
-  document.getElementById('debtor2Name').innerHTML = debitAccount[1].accountOwner;
-  document.getElementById('debtor2Account').innerHTML = debitAccount[1].accountNumber;
-  document.getElementById('debtor2PartyID').innerHTML = debitAccount[1].accountOwnerId;
-  document.getElementById('debtor2BIC').innerHTML = debitAccount[1].bankIdentifier;
+  document.getElementById('debtor2Name').innerHTML = debtorAccountString[1].accountOwner;
+  document.getElementById('debtor2Account').innerHTML = debtorAccountString[1].accountNumber;
+  document.getElementById('debtor2PartyID').innerHTML = debtorAccountString[1].accountOwnerId;
+  document.getElementById('debtor2BIC').innerHTML = debtorAccountString[1].bankIdentifier;
 };
 
 function completeAccount () {
-  document.getElementById('debtor1Name').innerHTML = debitAccount[0].accountOwner;
-  document.getElementById('debtor1Account').innerHTML = debitAccount[0].accountNumber;
-  document.getElementById('debtor1PartyID').innerHTML = debitAccount[0].accountOwnerId;
-  document.getElementById('debtor1BIC').innerHTML = debitAccount[0].bankIdentifier;
+  document.getElementById('debtor1Name').innerHTML = debtorAccountString[0].accountOwner;
+  document.getElementById('debtor1Account').innerHTML = debtorAccountString[0].accountNumber;
+  document.getElementById('debtor1PartyID').innerHTML = debtorAccountString[0].accountOwnerId;
+  document.getElementById('debtor1BIC').innerHTML = debtorAccountString[0].bankIdentifier;
 
-  document.getElementById('debtor2Name').innerHTML = debitAccount[1].accountOwner;
-  document.getElementById('debtor2Account').innerHTML = debitAccount[1].accountNumber;
-  document.getElementById('debtor2PartyID').innerHTML = debitAccount[1].accountOwnerId;
-  document.getElementById('debtor2BIC').innerHTML = debitAccount[1].bankIdentifier;
+  document.getElementById('debtor2Name').innerHTML = debtorAccountString[1].accountOwner;
+  document.getElementById('debtor2Account').innerHTML = debtorAccountString[1].accountNumber;
+  document.getElementById('debtor2PartyID').innerHTML = debtorAccountString[1].accountOwnerId;
+  document.getElementById('debtor2BIC').innerHTML = debtorAccountString[1].bankIdentifier;
 };
 
  completeAccount();
